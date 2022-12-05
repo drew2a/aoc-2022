dab41701b698f57aa69e047ea262d1717ed950e6
drew2a <for.drew@gmail.com>
2022-12-05 18:00:11 +0100

    Add day 4

---
+++ day 4/2/day4_2.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+pairs = [r.split(',') for r in Path('./input.txt').read_text().split('\n') if r]
+score = 0
+for p in pairs:
+    def _in(p1, p2):
+        return p1[0] <= p2[0] <= p1[1] or p1[0] <= p2[1] <= p1[1]
+
+
+    first = [int(i) for i in p[0].split('-')]
+    second = [int(i) for i in p[1].split('-')]
+    if _in(first, second) or _in(second, first):
+        score += 1
+
+print(score)  # 536 too low
